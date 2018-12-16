3b4f997d03266f105402af280d9a4589a3fe98bf
Saleh Aly <salehaly01@gmail.com>
2018-12-16 15:15:35 +0200

    Fixed requests quote type

--- src/api/getAuthors.js
+++ src/api/getAuthors.js
@@ -3,6 +3,6 @@ import axios from "./axiosInstance";
 export default function getAuthors() {
   return axios.request({
     method: "GET",
-    url: `/authors`
+    url: "/authors"
   });
 }
--- src/api/getCategories.js
+++ src/api/getCategories.js
@@ -3,6 +3,6 @@ import axios from "./axiosInstance";
 export default function getCategories() {
   return axios.request({
     method: "GET",
-    url: `/categories`
+    url: "/categories"
   });
 }
